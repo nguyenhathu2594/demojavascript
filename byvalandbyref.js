654c7fbdd36394810b7da341d71b5b749d23de06
nguyenhathu2594 <nguyenhathu2594@gmail.com>
2016-08-10 17:00:09 +0700

    commit byval

---
+++ byvalandbyref.js
@@ -0,0 +1,14 @@
+var flight = {status: "Flying"};
+
+//Object is passed by reference by default
+var byRefFlight = flight;
+
+//This code really copies object to other instance
+var byCopyFlight = Object.create(flight);
+
+byRefFlight.status = "Landing";
+console.log('flight.status = ' + flight.status + ". byRefFlight.status = " + byRefFlight.status);
+//flight.status = Landing. byRefFlight.status = Landing
+
+// byCopyFlight.status = "Crash!";
+console.log('flight.status = ' + flight.status + ". byCopyFlight.status = " + byCopyFlight.status);
