007b1a0d168fd23a2f67685749e9ad6b96f07e6a
nguyenhathu2594 <nguyenhathu2594@gmail.com>
2016-08-18 14:52:07 +0700

    a

--- Default Parameters/default-parameter.js
+++ Default Parameters/default-parameter.js
@@ -1,6 +1,3 @@
-/**
- * Created by ThuNH on 16/08/2016.
- */
 'use strict';
 //Khi khai báo tham số mặc định nên để sau tham số mình sẽ truyền vào để tránh lỗi
 // var add = function (a,b=2,c=4) {
@@ -11,23 +8,27 @@
 
 //Truyền mảng sử dụng Object
 
-// var dst = {foo:1, bar:2};
-// var src = {bar:3, foofoo:4};
-// var target = Object.assign({},dst,src);
-//
-// console.log(target);
+var dst = {foo:1, bar:2};
+var src = {bar:3, foofoo:4};
+var target = Object.assign({}, dst, src);
+
+console.log(target);
 
 //Truy cập lấy 1 biến trong 1 mảng
 
 //Khai báo mảng
 
-var person = {
-    name: {first:"Nguyễn", last:"Hà Thu"},
-    study: {cls: "Học NodeJS", school: "Tự học"},
-    rank: "Newbie"
-};
-
-//Lấy 1 phần tử
-
-var {rank} = person;
-console.log(rank);
+// var person = {
+//     name: {first:"Nguyễn", last:"Hà Thu"},
+//     study: {cls: "Học NodeJS", school: "Tự học"},
+//     rank: "Newbie"
+// };
+//
+// //Lấy 1 phần tử
+//
+// //Không chạy được
+// var {rank: myRank} = person;
+// console.log(myRank);
+// //Chạy được
+// var cls = person.study.cls;
+// console.log(cls);
